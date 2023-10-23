7431b4273346bc83e89d612fa748aba6cde1f94e
olohiperbolo <aleksanderopszala@gmail.com>
2023-10-23 14:26:01 +0200

    tworzenie funkcji sczytującej plik i naprawa funkcji zliczającej

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import matplotlib as plt
 
 quantity = []
 
+#zliczanie liter
 def count_letters(text):
+    quantity = {}
     for char in text:
         if char.isalpha():
             char = char.lower()
@@ -13,3 +15,28 @@ def count_letters(text):
                 quantity[char] = 1
     return quantity
 
+#tworzenie wykresu
+def draw_char(quantity):
+    letters = list(quantity.keys())
+    occurences = list(quantity.values())
+
+    plt.bar(letters, occurences)
+    plt.xlabel('litery')
+    plt.ylabel('il. wystapien')
+    plt.title("HISTOGRAM")
+    plt.show()
+
+#funkcja do wpisywania lokalizacji pliku
+def load_file(filename):
+    with open(filename, 'r') as file:
+        text = file.read()
+        return text
+if __name__ == "__main__":
+    file_path = input("Podaj ścieżkę: ")
+
+    try:
+        text = load_file(file_path)
+        letter_quantity = count_letters(text)
+        draw_char(letter_quantity)
+    except FileNotFoundError:
+        print("Plik nie został znaleziony.")
