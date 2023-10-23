0a1fab0dfc31ff80d55d4fad7ff1a0d2e7472dad
olohiperbolo <aleksanderopszala@gmail.com>
2023-10-23 23:49:03 +0200

    dodanie metod, prawidłowe działanie programu

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 import codecs
 import os
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
-with codecs.open('tekst1.txt', 'r', encoding='utf=8') as f:
+def count_letters(text):
+    letters = 'aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż'
+    letters_dictionary = {letter: 0 for letter in letters}
+
+    for char in text.lower():
+        if char in letters:
+            letters_dictionary[char] += 1
+
+    return letters_dictionary
+
+def draw_histogram(letters_dict):
+    letters = list(letters_dict.keys())
+    occurrences = list(letters_dict.values())
+
+    plt.bar(letters, occurrences)
+    plt.xlabel('litery')
+    plt.ylabel('il. wystapien')
+    plt.title("HISTOGRAM")
+    plt.show()
+
+with codecs.open('tekst1.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
-letters = 'aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż'
+letter_counts = count_letters(text)
+draw_histogram(letter_counts)
 
-letters_dictionary = {letter: 0 for letter in letters}
 
-for char in text.lower():
-    if char in letters:
-        letters_dictionary[char] += 1
-
-total_num_letters = sum(letters_dictionary.values())
+'''total_num_letters = sum(letters_dictionary.values())
 sorted_pairs = sorted([(value, key) for (key, value) in letters_dictionary.items()], reverse=True)
 
 for value, letter in sorted_pairs:
     percentage = 100*value/total_num_letters
-    print(f'{letter}:{value}\t({percentage:.2f}%)')
-
-f.close()
+    print(f'{letter}:{value}({percentage:.2f}%)')'''
