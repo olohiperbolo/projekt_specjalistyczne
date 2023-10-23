4a3d8a6d9f37940903dfbfb7e614a9cb19df2777
olohiperbolo <aleksanderopszala@gmail.com>
2023-10-23 17:20:41 +0200

    duze uproszczenie i naprawienie

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
+import codecs
 import os
 import matplotlib as plt
 
-text = 'dzien dobry piotruciu nie dawaj sie, dzien dobry piotrusiu badz dzielny'
+with codecs.open('tekst1.txt', 'r', encoding='utf=8') as f:
+    text = f.read()
 
-letters = 'abcdefghijklmnopqrstuvwxyz'
+letters = 'aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż'
 
 letters_dictionary = {letter: 0 for letter in letters}
-
-#print(letters_dictionary)
 
 for char in text.lower():
     if char in letters:
         letters_dictionary[char] += 1
 
-'''total_num_letter = 0
-for letter in letters:
-    total_num_letter += letters_dictionary[letter]'''
-
 total_num_letters = sum(letters_dictionary.values())
 sorted_pairs = sorted([(value, key) for (key, value) in letters_dictionary.items()], reverse=True)
-
-#print(sorted_pairs)
 
 for value, letter in sorted_pairs:
     percentage = 100*value/total_num_letters
     print(f'{letter}:{value}\t({percentage:.2f}%)')
+
+f.close()
