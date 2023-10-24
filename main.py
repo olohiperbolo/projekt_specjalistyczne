e4d2828c86be47a52e344f4084017fc71cf8fca6
olohiperbolo <aleksanderopszala@gmail.com>
2023-10-24 21:19:17 +0200

    proba dodania histogramu .jpg

--- main.py
+++ main.py
@@ -1,33 +1,45 @@
 import os
 import matplotlib.pyplot as plt
+import cv2 as cv
+
+choice = input('do czego chcesz histogram: 1 - plik.txt, 2 - zdjecie.jpg: ')
 
 file_path = input("Podaj ścieżkę do pliku: ")
-def count_letters(text):
-    letters = 'aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż'
-    letters_dictionary = {letter: 0 for letter in letters}
 
-    for char in text.lower():
-        if char in letters:
-            letters_dictionary[char] += 1
+if(choice == '1'):
+    def count_letters(text):
+        letters = 'aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż'
+        letters_dictionary = {letter: 0 for letter in letters}
 
-    return letters_dictionary
+        for char in text.lower():
+            if char in letters:
+                letters_dictionary[char] += 1
 
-def draw_histogram(letters_dict):
-    letters = list(letters_dict.keys())
-    occurrences = list(letters_dict.values())
+        return letters_dictionary
 
-    plt.bar(letters, occurrences)
-    plt.xlabel('litery')
-    plt.ylabel('il. wystapien')
-    plt.title("HISTOGRAM")
-    plt.show()
 
-try:
-    with open(file_path, 'r') as f:
-        text = f.read()
-except FileNotFoundError:
-    print("Błędna ścieżką, bądź plik nie istnieje")
+    def draw_histogram(letters_dict):
+        letters = list(letters_dict.keys())
+        occurrences = list(letters_dict.values())
+
+        plt.bar(letters, occurrences)
+        plt.xlabel('litery')
+        plt.ylabel('il. wystapien')
+        plt.title("HISTOGRAM")
+        plt.show()
+
+
+    try:
+        with open(file_path, 'r') as f:
+            text = f.read()
+    except FileNotFoundError:
+        print("Błędna ścieżką, bądź plik nie istnieje")
+    else:
+        letter_counts = count_letters(text)
+        draw_histogram(letter_counts)
+elif choice == '2':
+    img = cv.imread(file_path, 0)
+    hist = cv.calcHist([img], [0], None, [256], [0, 256])
 else:
-    letter_counts = count_letters(text)
-    draw_histogram(letter_counts)
+    print('niepoprawna wartość')
 
