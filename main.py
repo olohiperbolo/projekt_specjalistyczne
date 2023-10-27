b5cb500281dbf01712db8fdc81957d02450f4430
SuchyHere <michalsuchecki3@gmail.com>
2023-10-27 22:46:25 +0200

    dodanie graficznego menu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import matplotlib.pyplot as plt
+import tkinter as ttk
 
 def count_letters(text):
     letters = 'aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż'
@@ -22,33 +23,57 @@ def draw_histogram(letters_dict):
     plt.show()
 
 running = True
+def menu_window():
+    global root
+    root = ttk.Tk()
+    root.geometry('400x200')
+    frame1 = ttk.Frame(root, width=400, height=100)
+    frame2 = ttk.Frame(root, width=400, height=100)
+    from_text_button = ttk.Button(width= 15,text='Z Podanego tekstu', command=lambda:[frame2.forget, place_entry()])
+    from_text_button.place(x=25,y=110)
+    change_file = ttk.Button(width= 16,text='przetworz plik tekstowy',  command=lambda:[frame1.forget, place_entry1()])
+    change_file.place(x=207,y=110)
+    root.mainloop()
 
-while running:
-    print("Wybierz opcję:")
-    print("1. Podaj tekst")
-    print("2. Przetwórz plik tekstowy")
-    print("3. Wyjdź")
+def place_entry():
+    global frame1,frame2, from_text_entry
+    frame1 = ttk.Frame(root, width=400, height=100)
+    frame2 = ttk.Frame(root, width=400, height=100)
+    frame1.place(x=0,y=0)
+    from_text_entry = ttk.Entry(frame1, width=20, background='white',foreground='black')
+    from_text_entry.place(x=103,y=60)
+    from_file_label = ttk.Label(frame1, text='Podaj tekst')
+    from_file_label.place(x=160, y=25)
+    button = ttk.Button(root, text='Dalej', command=lambda:[from_text()])
+    button.place(x=170, y = 150)
+def place_entry1():
+    global frame1,frame2, from_file_entry
+    frame1 = ttk.Frame(root, width=400, height=100)
+    frame2 = ttk.Frame(root, width=400, height=100)
+    frame1.place(x=0,y=0)
+    from_file_entry = ttk.Entry(frame1, width=20, background='white', foreground='black')
+    from_file_entry.place(x=103, y=60)
+    from_file_label = ttk.Label(frame1, text='Podaj sciezke pliku')
+    from_file_label.place(x=140, y=25)
+    button = ttk.Button(root, text='Dalej', command=lambda:[from_file()])
+    button.place(x=170, y = 150)
 
-    choice = input("Twój wybór: ")
+def from_text():
+    text = from_text_entry.get()
+    letter_counts = count_letters(text)
+    draw_histogram(letter_counts)
 
-    if choice == '1':
-        text = input("Wprowadź tekst: ")
+def from_file():
+    running = True
+    try:
+        file_path = from_file_entry.get()
+        with open(file_path, 'r') as f:
+            text = f.read()
+    except FileNotFoundError:
+        print("Błędna ścieżka, bądź plik nie istnieje")
+    else:
         letter_counts = count_letters(text)
         draw_histogram(letter_counts)
 
-    elif choice == '2':
-        file_path = input("Podaj ścieżkę do pliku: ")
 
-        try:
-            with open(file_path, 'r') as f:
-                text = f.read()
-        except FileNotFoundError:
-            print("Błędna ścieżka, bądź plik nie istnieje")
-        else:
-            letter_counts = count_letters(text)
-            draw_histogram(letter_counts)
-
-    elif choice == '3':
-        running = False
-else:
-    print('Niepoprawna wartość. Podaj 1, 2 lub 3:')
+menu_window()
